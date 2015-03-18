f933cd8f33efd43c27010980d673fea27775a602
raub <raubvogel@gmail.com>
2015-03-18 14:18:10 -0400

    Now we can read arguments from command line

--- README.md
+++ README.md
@@ -5,10 +5,18 @@ Why one hit? Think of a one hit wonder: this servers exactly one file,
 as in you can request anything you want and will only get back the
 file configured in the code (or fed to it). 
 
-This was written as a quick way to provide a 
+This Python script was written as a quick way to provide a 
 kickstarter(RedHat/CentOS)/preseed(Debian/Ubuntu) config file without having 
 to put together a proper webserver. Of course, in a large scale setup you do
 use a real webserver which selects the right config file, but this is not
 that.
 
+It is also a way for me to learn a bit more of Python classes by putting
+together a tiny (can't go smaller than a single page, right) website.
+The original version has all arguments hard coded while the current one
+allows you to enter the server's IP and port, and the filename/path from
+command line.
 
+Why would you want to enter the IP? If you enter the IP, it means it will
+only listen on that interface/IP. If you enter nothing ("" so it counts
+it as an argument), it will listen on all interfaces.
--- onehitweb.py
+++ onehitweb.py
@@ -1,21 +1,61 @@
 #! /usr/bin/env python
 '''
-Simple dumb webserver to serve a file.
-It will try to serve a file called preseed.cfg, located in the directory
-program was called, on localhost:8000
+Simple dumb webserver to serve a file. 
+If no arguments are provided, it will try to serve a file called preseed.cfg,
+located in the directory program was called, on localhost:8000
 
-The idea is you can ask for any file you want, and will get what we
-give to you.
-
-Based on https://wiki.python.org/moin/BaseHttpServer
+Based on https://wiki.python.org/moin/BaseWebServer
 http://www.acmesystems.it/python_httpserver
-'''
-import time
-import BaseHTTPServer
 
-HOST_NAME = '' # Accept requests on all interfaces
-PORT_NUMBER = 8000
+Useful
+http://stackoverflow.com/questions/18444395/basehttprequesthandler-with-custom-instance
+http://www.mlsite.net/blog/?p=80
+
+http://stackoverflow.com/questions/237432/python-properties-and-inheritance
+https://www.safaribooksonline.com/library/view/python-cookbook-3rd/9781449357337/ch08s08.html
+http://www.opensource.apple.com/source/python/python-3/python/Lib/BaseHTTPServer.py
+http://stackoverflow.com/questions/8333354/how-to-override-constructor-of-python-class-with-many-arguments
+
+'''
+
+import time
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import getopt
+import sys
+
+# Some default values in case they are not passed as arguments
+HOST_NAME = '' 
+PORT_NUMBER = 8000 
 FILE = 'preseed.cfg'
+
+class MyHandler(BaseHTTPRequestHandler):
+    '''
+    Extending __init__ so we can pass file
+    '''
+    def __init__(self, filename, *args):
+        self.file = read_file(filename)
+        BaseHTTPRequestHandler.__init__(self, *args)
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(self.file)
+	return
+
+class WebServer:
+    def __init__(self, filename, host, port):
+        def handler(*args):
+            MyHandler(filename, *args)
+        self.server = HTTPServer((host, port), handler)
+
+    def start(self):
+        try:
+            self.server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+        self.server.server_close()
+
 
 def read_file(filename):
     '''
@@ -24,21 +64,16 @@ def read_file(filename):
     file = open(filename, "r")
     return file.read()
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_GET(self):
-        file = read_file(FILE)
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(file)
+def print_usage():
+    print "Usage:"
+    print __file__ + " [filename] [host] [port]"
+
+def main(filename = FILE, host = HOST_NAME, port = PORT_NUMBER):
+    print "in main, file =", filename
+    httpd = WebServer(filename, host, port)
+    print time.asctime(), "Server Starts - %s:%s" % (host, port)
+    httpd.start()
+    print time.asctime(), "Server Stops - %s:%s" % (host, port)
 
 if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
+   main(*sys.argv[1:])
